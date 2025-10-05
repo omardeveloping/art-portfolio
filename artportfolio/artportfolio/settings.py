02a61c0260786562c9401663181d9e9a7f50d536
omardeveloping <omar.montanares@inacapmail.cl>
2025-10-05 19:14:31 -0300

    Environment variables

--- artportfolio/artportfolio/settings.py
+++ artportfolio/artportfolio/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!a@u%#861)v1vt!@2gxdzo34f3d=2@b!h+kizu%s)6#!o8p6+$'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
 
 
 # Application definition
