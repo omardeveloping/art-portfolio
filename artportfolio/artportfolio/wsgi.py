48f93f1145d19662877059fdd1c6881785ff6a91
omardeveloping <omar.montanares@inacapmail.cl>
2026-02-08 18:01:31 -0300

    solucion 500

--- artportfolio/artportfolio/wsgi.py
+++ artportfolio/artportfolio/wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
+from pathlib import Path
+
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
+dotenv_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path)
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'artportfolio.settings')
 
 application = get_wsgi_application()
